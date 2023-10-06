e548d1c44f23e9fd662b539d7056e6279cace91e
Pavlo Opanasenko <pavloopanasenko@Air-Pavlo.home>
2023-10-05 18:50:18 -0700

    refactoring

--- module/grid-prise/GridPrise.js
+++ module/grid-prise/GridPrise.js
@@ -21,17 +21,18 @@ export default class {
                     <picture class="prises__image">
                         <img src="./images/price-image/${i}.jpeg" loading="lazy" alt="image">
                     </picture>
+                    <div class="prises__section"></div>
                 </div>
             `);
             this.data[i].forEach(elem => {
-                let card = ce(`
-                    <div class="prises__section">
+                let card = `
+                    <div class="section__item">
                         <h3 class="prises__information prises__information_title">${elem.name}</h3>
                         <h4 class="prises__information prises__information_subtitle">${elem.additionally}</h4>
-                        <span class="prises__information prises__information_price">${elem.price}</span>
+                        <span class="prises__information prises__information_price">${elem.price} $</span>
                     </div>
-               `);
-                this.card.append(card);
+               `;
+                this.card.querySelector('.prises__section').innerHTML += (card);
             });
             this.elem.append(this.card);
         }
